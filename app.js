7faecb4717e5a055bae6129303c3601d8a9b5cfb
Alisiyad9 <alisiyad9@gmail.com>
2021-05-05 01:25:35 +0100

    5/5

--- app.js
+++ app.js
@@ -23,3 +23,39 @@ console.log('slice, splice and split');
 
 console.log(divideName('how are u doing'));
 console.log('asa');
+
+// var carouselIndex = 0;
+// carouselSlides();
+
+// function carouselSlides() {
+//   var carousel = document.getElementsByClassName('imageScrol');
+//   for (var i = 0; i < carousel.length; i++) {
+//     carousel[i].style.display = 'none';
+//   }
+//   carouselIndex++;
+//   if (carouselIndex > carousel.length) {
+//     carouselIndex = 1;
+//   }
+//   carousel[carouselIndex - 1].style.display = 'block';
+//   setTimeout(carouselSlides, 3000);
+// }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
