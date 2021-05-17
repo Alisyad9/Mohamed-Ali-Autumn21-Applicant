3461fe8ef2f432a3a988e285237e321505688f95
Alisiyad9 <alisiyad9@gmail.com>
2021-05-17 02:25:01 +0100

    done

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-function divideName(array) {
-  let result = array.split(' ');
-  // console.log(result);
-  // result.forEach((element) => {
-  //   result.push(element);
-  //   console.log(element);
-  // });
-  let count = 0;
-  for (let i = 0; i < result.length; i++) {
-    console.log(result[i]);
+const hamburgerBar = document.querySelector(
+  '.header .navbar-class .nav-list .hamburger-bar'
+);
+const mobileBar = document.querySelector('.header .navbar-class .nav-list ul');
 
-    if (result[i].length > count) {
-      count = result[i].length;
-    }
-    return count;
-  }
-  console.log(count);
-  return count;
-}
-let name = [2, 'hello', 3];
+const header = document.querySelector('.header .container');
 
-console.log('slice, splice and split');
+hamburgerBar.addEventListener('click', () => {
+  hamburgerBar.classList.toggle('active');
+  mobileBar.classList.toggle('active');
+});
 
-console.log(divideName('how are u doing'));
-console.log('asa');
+// function divideName(array) {
+//   let result = array.split(' ');
+//   // console.log(result);
+//   // result.forEach((element) => {
+//   //   result.push(element);
+//   //   console.log(element);
+//   // });
+//   let count = 0;
+//   for (let i = 0; i < result.length; i++) {
+//     console.log(result[i]);
+
+//     if (result[i].length > count) {
+//       count = result[i].length;
+//     }
+//     return count;
+//   }
+//   console.log(count);
+//   return count;
+// }
+// let name = [2, 'hello', 3];
+
+// console.log('slice, splice and split');
+
+// console.log(divideName('how are u doing'));
+// console.log('asa');
 
 // var carouselIndex = 0;
 // carouselSlides();
